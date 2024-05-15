7664eb72551b0bf418f17a8d3aa07b856d568fea
Your Name <you@example.com>
2024-05-15 21:17:14 +0300

    Deleted models.go and modified main.go

--- main.go
+++ main.go
@@ -12,14 +12,6 @@ func main() {
 		return
 	}
 
-	// c2bRequest := C2B{
-	// 	ShortCode:     "600981",
-	// 	CommandID:     "CustomerPayBillOnline",
-	// 	Amount:        "100",
-	// 	Msisdn:        "254719453367",
-	// 	BillRefNumber: "123456",
-	// }
-
 	// fmt.Println("Executing C2B payment...")
 	// C2BPayment(token)
 	// time.Sleep(2 * time.Second)
@@ -28,21 +20,9 @@ func main() {
 	// 	return
 	// }
 
-	// // b2cRequest := B2C{
-	// // 	InitiatorName:      "testapi",
-	// // 	SecurityCredential: "R5yTD7xxAedBhIzeeDuKmsdGwVWMTyBQ7lrNirObp0DKIM0XXcJhBgOzWq3LjUGqQcLqrYOWvnAf",
-	// // 	CommandID:          "BusinessPayment",
-	// // 	Amount:             1,
-	// // 	PartyA:             600996,
-	// // 	PartyB:             254719453367,
-	// // 	Remarks:            "Okay",
-	// // 	QueueTimeOutURL:    "https://mydomain.com/b2c/queue",
-	// // 	ResultURL:          "https://mydomain.com/b2c/result",
-	// // }
+	fmt.Println("Executing B2C payment...")
+	B2CPayment(token)
 
-	// fmt.Println("Executing B2C payment...")
-	// B2CPayment(token)
-
-	fmt.Println("Pushing STK...")
-	simulate(token)
+	// fmt.Println("Pushing STK...")
+	// simulate(token)
 }
